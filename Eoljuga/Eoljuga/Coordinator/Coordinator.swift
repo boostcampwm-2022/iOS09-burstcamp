618d4fa9583fc3b1cdced1875b8b51b457273043
never-better <youtaktak@gmail.com>
2022-11-15 17:39:44 +0900

    feat: Coordinator 프로토콜

---
+++ Eoljuga/Eoljuga/Coordinator/Coordinator.swift
@@ -0,0 +1,34 @@
+//
+//  Coordinator.swift
+//  Eoljuga
+//
+//  Created by youtak on 2022/11/15.
+//
+
+import UIKit
+
+enum CoordinatorType {
+    case app
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinator: Coordinator)
+}
+
+protocol Coordinator: AnyObject {
+    var childCoordinator: [Coordinator] { get set }
+    var navigationCotroller: UINavigationController { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    var type: CoordinatorType { get }
+
+    init(_ navigationController: UINavigationController)
+
+    func start()
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinator.removeAll()
+        finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+}
