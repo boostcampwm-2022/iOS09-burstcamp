26d0733c8ae68f4b542b753d3210882ddd18ee28
miniOS <seungmin.oh.dev@gmail.com>
2022-11-15 15:59:03 +0900

    START

---
+++ Eoljuga/Eoljuga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eoljuga
+//
+//  Created by SEUNGMIN OH on 2022/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eoljuga/EoljugaUITests/EoljugaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EoljugaUITestsLaunchTests.swift
+//  EoljugaUITests
+//
+//  Created by SEUNGMIN OH on 2022/11/15.
+//
+
+import XCTest
+
+final class EoljugaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
