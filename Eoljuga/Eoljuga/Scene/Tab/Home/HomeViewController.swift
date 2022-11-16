dec6c1db825c890ad4fb317220662f6e9dbeebf5
never-better <youtaktak@gmail.com>
2022-11-16 20:27:02 +0900

    feat: defaultCollectionView 추가
    기본 피드에 사용할 collectionView

--- Eoljuga/Eoljuga/Scene/Tab/Home/HomeViewController.swift
+++ Eoljuga/Eoljuga/Scene/Tab/Home/HomeViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class HomeViewController: UIViewController {
+
+    lazy var defaultFeedCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewFlowLayout()
+    ).then {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        layout.sectionInset = .zero
+        $0.collectionViewLayout = layout
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: 지우기
-        view.backgroundColor = .systemPink
+        configure()
+    }
+
+    private func configure() {
+        configureView()
+        configureDefaultFeedCollectionView()
+    }
+
+    private func configureView() {
+        view.backgroundColor = .white
+    }
+
+    private func configureDefaultFeedCollectionView() {
+        view.addSubview(defaultFeedCollectionView)
+        defaultFeedCollectionView.backgroundColor = .orange
+        defaultFeedCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
