91d673f29c17410477172924d795c508c016e2a1
never-better <youtaktak@gmail.com>
2022-11-16 12:57:24 +0900

    feat: Mypage 로그아웃 버튼 추가

--- Eoljuga/Eoljuga/Tab/MyPage/MyPageViewController.swift
+++ Eoljuga/Eoljuga/Tab/MyPage/MyPageViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
+    lazy var logoutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그아웃", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(logoutButtonTouched), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: 지우기
         view.backgroundColor = .systemBrown
+        configureLogoutButton()
         // Do any additional setup after loading the view.
+    }
+
+    func configureLogoutButton() {
+        view.addSubview(logoutButton)
+        logoutButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(44)
+            $0.center.equalToSuperview()
+        }
+    }
+
+    @objc func logoutButtonTouched() {
+        print("버튼 터치")
     }
 }
