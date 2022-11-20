19b2ab2429a63da4f6908e692fc1b03058c0d76c
neuli <asdfz888@naver.com>
2022-11-20 21:46:43 +0900

    feat: FireStoreConfiguration 생성 (path, queryParameter) #58

---
+++ Eoljuga/Eoljuga/Util/Constant/FireStoreConfiguration.swift
@@ -0,0 +1,37 @@
+//
+//  FireStoreConfiguration.swift
+//  Eoljuga
+//
+//  Created by neuli on 2022/11/17.
+//
+
+import Foundation
+
+enum FireStoreURL {
+    static let baseURL = "https://firestore.googleapis.com/v1/projects/eoljuga-9b868"
+    static let documentURL = "/databases/(default)/documents"
+    static let runQuery = ":runQuery"
+    static let commit = ":commit"
+}
+
+enum FireStoreCollection: String {
+    case user = "User"
+    case blog = "Blog"
+    case feed = "Feed"
+
+    var path: String {
+        switch self {
+        case .user: return "/\(FireStoreCollection.user.rawValue)"
+        case .blog: return "/\(FireStoreCollection.blog.rawValue)"
+        case .feed: return "/\(FireStoreCollection.feed.rawValue)"
+        }
+    }
+}
+
+enum FireStoreField {
+    static let fields = "fields"
+}
+
+enum QueryParameter {
+    static let documentID = "?documentId="
+}
