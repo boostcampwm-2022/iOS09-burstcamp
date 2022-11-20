3a32f667cbef98ff641852094ae27501b2b2fd4a
neuli <asdfz888@naver.com>
2022-11-20 21:47:21 +0900

    feat: network 통신 #57 #58

---
+++ Eoljuga/Eoljuga/Util/Constant/NetworkError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkError.swift
+//  FireStoreTest
+//
+//  Created by neuli on 2022/11/20.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case error(statusCode: Int)
+    case urlError
+    case decodeError
+    case unknown
+    case unknownMessage(_ message: String)
+}
