7eb9b7c2aecc86a4b2871ecc2c97d70c6f099e94
never-better <youtaktak@gmail.com>
2022-11-24 19:50:44 +0900

    feat: Feed 모델 추가

---
+++ burstcamp/burstcamp/Model/Feed.swift
@@ -0,0 +1,18 @@
+//
+//  Feed.swift
+//  burstcamp
+//
+//  Created by youtak on 2022/11/24.
+//
+
+import Foundation
+
+struct Feed {
+    let writerUUID: String
+    let blogUUID: String
+    let title: String
+    let pubDate: Date
+    let url: String
+    let thumbnail: String
+    let content: String
+}
