1531ac00317e797100fdacada89532ad7e97d140
neuli <asdfz888@naver.com>
2022-11-24 20:21:51 +0900

    feat: 네비게이션, 탭바 배경색 iOS 15 이상 대응 #107

--- burstcamp/burstcamp/Util/Extension/UI/UINavigationController+configure.swift
+++ burstcamp/burstcamp/Util/Extension/UI/UINavigationController+configure.swift
@@ -12,5 +12,14 @@ extension UINavigationController {
     convenience init(backgroundColor: UIColor = .background) {
         self.init()
         self.navigationBar.backgroundColor = backgroundColor
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithDefaultBackground()
+            navigationBarAppearance.backgroundColor = backgroundColor
+//            navigationBar.standardAppearance = navigationBarAppearance
+//            navigationBar.scrollEdgeAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        }
     }
 }
