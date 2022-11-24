ccbf269a966f290500220bc3704d3cce0d68f221
neuli <asdfz888@naver.com>
2022-11-24 14:25:09 +0900

    feat: DefaultImageLabel 파라미터 추가

--- burstcamp/burstcamp/Scene/Common/View/DefaultImageLabel.swift
+++ burstcamp/burstcamp/Scene/Common/View/DefaultImageLabel.swift
@@ -9,63 +9,92 @@ import UIKit
 
 final class DefaultImageLabel: UILabel {
 
-    private let fontAttribute: [NSAttributedString.Key: Any] = [
-        .font: UIFont.regular12
-    ]
-
-    private let colorAttribute: [NSAttributedString.Key: Any] = [
-        .foregroundColor: UIColor.systemGray2
-    ]
-
-    private lazy var attributes: [NSAttributedString.Key: Any] = fontAttribute
-        .merging(colorAttribute) { _, new in new }
-
     init(
+        frame: CGRect = .zero,
         icon: UIImage?,
-        text: String
+        iconBounds: CGRect = CGRect(
+            x: Constant.zero,
+            y: Constant.spaceMinus2,
+            width: Constant.space16,
+            height: Constant.space12
+        ),
+        iconColor: UIColor = .systemGray2,
+        text: String,
+        font: UIFont = .regular12,
+        textColor: UIColor = .systemGray2
     ) {
-        super.init(frame: .zero)
-        attributedText = attributeString(icon: icon, text: text)
+        super.init(frame: frame)
+        attributedText = attributeString(icon: icon,
+                                         iconColor: iconColor,
+                                         iconBounds: iconBounds,
+                                         text: text,
+                                         font: font,
+                                         textColor: textColor
+        )
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func attributeString(icon: UIImage?, text: String) -> NSAttributedString {
+    private func attributeString(
+        icon: UIImage?,
+        iconColor: UIColor,
+        iconBounds: CGRect,
+        text: String,
+        font: UIFont = .regular12,
+        textColor: UIColor
+    ) -> NSAttributedString {
         let attributeString = NSMutableAttributedString()
 
         if let icon = icon {
-            let imageAttributeString = imageAttributeString(icon: icon)
+            let imageAttributeString = imageAttributeString(
+                icon: icon,
+                iconColor: iconColor,
+                iconBounds: iconBounds
+            )
             attributeString.append(imageAttributeString)
             attributeString.append(NSAttributedString(string: " "))
         }
 
-        let textAttributeString = textAttributeString(text: text)
+        let textAttributeString = textAttributeString(
+            text: text,
+            font: font,
+            textColor: textColor
+        )
         attributeString.append(textAttributeString)
         return attributeString
     }
 
-    private func imageAttributeString(icon: UIImage?) -> NSAttributedString {
+    private func imageAttributeString(
+        icon: UIImage?,
+        iconColor: UIColor,
+        iconBounds: CGRect
+    ) -> NSAttributedString {
         let imageAttachment = NSTextAttachment().then {
             $0.image = icon?
-                .withTintColor(.systemGray2, renderingMode: .alwaysOriginal)
-            $0.bounds = CGRect(
-                x: Constant.zero,
-                y: Constant.spaceMinus2,
-                width: Constant.space16,
-                height: Constant.space12
-            )
+                .withTintColor(iconColor, renderingMode: .alwaysOriginal)
+            $0.bounds = iconBounds
         }
         return NSAttributedString(attachment: imageAttachment)
     }
 
-    private func textAttributeString(text: String) -> NSAttributedString {
+    private func textAttributeString(
+        text: String,
+        font: UIFont,
+        textColor: UIColor
+    ) -> NSAttributedString {
         let textAttributeString = NSMutableAttributedString(
             string: text
         )
-        textAttributeString.addAttributes(
-            attributes,
+        textAttributeString.addAttribute(
+            .font,
+            value: font,
+            range: NSRange(location: 0, length: text.count)
+        )
+        textAttributeString.addAttribute(
+            .foregroundColor,
+            value: textColor,
             range: NSRange(location: 0, length: text.count)
         )
         return textAttributeString
