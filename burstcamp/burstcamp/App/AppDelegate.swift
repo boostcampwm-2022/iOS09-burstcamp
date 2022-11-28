f86e8b2885cf32bc06c16edd86385047111c0d74
neuli <asdfz888@naver.com>
2022-11-28 20:23:49 +0900

    feat: push알림 설정, FCM token 받아오기 #28

--- burstcamp/burstcamp/App/AppDelegate.swift
+++ burstcamp/burstcamp/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+import Firebase
 import FirebaseCore
 
 @main
@@ -17,6 +18,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         FirebaseApp.configure()
+        configurePushNotification(application)
+        configureMessaging()
         return true
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    private func configurePushNotification(_ application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions) { _, _ in}
+
+        application.registerForRemoteNotifications()
+    }
+}
+
+// MARK: - MessagingDelegate
+
+extension AppDelegate: MessagingDelegate {
+    private func configureMessaging() {
+        Messaging.messaging().delegate = self
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
+        }
+    }
+
+    // token refresh monitoring
+    func messaging(
+        _ messaging: Messaging,
+        didReceiveRegistrationToken fcmToken: String?
+    ) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        // TODO: update token
     }
 }
