3407cd783a398c80fedb2e931e4c88c8996f8d0d
neuli <asdfz888@naver.com>
2022-11-29 19:09:09 +0900

    feat: 블로그 주소 validation #34

---
+++ burstcamp/burstcamp/Util/Validation/Validation.swift
@@ -0,0 +1,30 @@
+//
+//  Validation.swift
+//  burstcamp
+//
+//  Created by neuli on 2022/11/29.
+//
+
+import Combine
+import Foundation
+
+struct Validation {
+    static func validate(name: String) -> Bool {
+        return name.isEmpty ? false : true
+    }
+
+    static func validate(camperID: String) -> Bool {
+        return camperID.count == 3 ? true : false
+    }
+
+    static func validateRegexp(blogLink: String) -> Bool {
+        let tistoryRegexp = #"^https://?[a-z0-9-]{4,32}.tistory.com$"#
+        let velogRegexp = #"^https://velog.io/@?[A-Za-z0-9-_]{3,16}$"#
+        if blogLink.range(of: tistoryRegexp, options: .regularExpression) != nil {
+            return true
+        } else if blogLink.range(of: velogRegexp, options: .regularExpression) != nil {
+            return true
+        }
+        return false
+    }
+}
