4aae4bec8b5d6a1c56f37a52c4160ede6143333e
neuli <asdfz888@naver.com>
2022-11-30 20:46:25 +0900

    feat: 임시 user Firebase

--- burstcamp/burstcamp/Model/User.swift
+++ burstcamp/burstcamp/Model/User.swift
@@ -19,4 +19,44 @@ struct User: Codable {
     let scrapFeedUUIDs: [String]
     let signupDate: Date
     let isPushOn: Bool
+    init(
+        userUUID: String,
+        nickname: String,
+        profileImageURL: String,
+        domain: Domain,
+        camperID: String,
+        ordinalNumber: Int,
+        blogURL: String,
+        blogTitle: String,
+        scrapFeedUUIDs: [String],
+        signupDate: Date,
+        isPushOn: Bool
+    ) {
+        self.userUUID = userUUID
+        self.nickname = nickname
+        self.profileImageURL = profileImageURL
+        self.domain = domain
+        self.camperID = camperID
+        self.ordinalNumber = ordinalNumber
+        self.blogURL = blogURL
+        self.blogTitle = blogTitle
+        self.scrapFeedUUIDs = scrapFeedUUIDs
+        self.signupDate = signupDate
+        self.isPushOn = isPushOn
+    }
+    
+    init(dict: [String: Any]) {
+        self.userUUID = dict["userUUID"] as? String ?? ""
+        self.nickname = dict["nickname"] as? String ?? ""
+        self.profileImageURL = dict["profileImageURL"] as? String ?? ""
+        let domainString = dict["domain"] as? String ?? "iOS"
+        self.domain = Domain(rawValue: domainString) ?? .iOS
+        self.camperID = dict["camperID"] as? String ?? ""
+        self.ordinalNumber = dict["ordinalNumber"] as? Int ?? 7
+        self.blogURL = dict["blogURL"] as? String ?? ""
+        self.blogTitle = dict["blogTitle"] as? String ?? ""
+        self.scrapFeedUUIDs = dict["scrapFeedUUIDs"] as? [String] ?? []
+        self.signupDate = dict["signupDate"] as? Date ?? Date() // TODO: Timestamp
+        self.isPushOn = dict["isPushOn"] as? Bool ?? false
+    }
 }
---
+++ burstcamp/burstcamp/Service/FirestoreUser.swift
@@ -0,0 +1,55 @@
+//
+//  FirestoreUser.swift
+//  burstcamp
+//
+//  Created by neuli on 2022/11/29.
+//
+
+import Combine
+import Foundation
+
+import Firebase
+import FirebaseFirestore
+
+struct FirebaseUser {
+    private static let userPath = Firestore.firestore().collection(FireStoreCollection.user.path)
+    private static let userUUIDField = "userUUID"
+    
+    static func save(user: User) -> AnyPublisher<User, FirestoreError> {
+        let path = userPath.document(user.userUUID)
+        guard let dictionary = user.asDictionary else {
+            return Fail(error: FirestoreError.failAsDictionary).eraseToAnyPublisher()
+        }
+        return Future<User, FirestoreError> { promise in
+            path.setData(dictionary) { error in
+                if error != nil {
+                    promise(.failure(FirestoreError.setDataError))
+                    return
+                }
+                promise(.success(user))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    static func fetch(userUUID: String) -> AnyPublisher<User, FirestoreError> {
+        return Future<User, FirestoreError> { promise in
+            userPath.document(userUUID)
+                .getDocument { document, error in
+                    if error != nil {
+                        promise(.failure(FirestoreError.setDataError))
+                        return
+                    }
+                    if let document = document,
+                       document.exists,
+                       let dict = document.data() {
+                        let user = User(dict: dict)
+                        promise(.success(user))
+                    } else {
+                        promise(.failure(FirestoreError.noDataError))
+                    }
+                }
+        }
+        .eraseToAnyPublisher()
+    }
+}
