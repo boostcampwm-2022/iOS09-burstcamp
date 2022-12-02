02cb19f1ff89b8c696e2d13236684b88a11e56b5
miniOS <seungmin.oh.dev@gmail.com>
2022-12-02 23:14:14 +0900

    feat: recommendFeed 수집 구현

--- Firebase-Functions/functions/index.js
+++ Firebase-Functions/functions/index.js
@@ -1,18 +1,22 @@
 import { initializeApp, getApps } from 'firebase-admin/app';
 import { pubsub, https } from 'firebase-functions';
 import { getBlogTitle } from './service/feedAPI.js';
-import { updateFeedDB } from './service/firestoreManager.js'
+import { updateFeedDB, updateRecommendFeedDB } from './service/firestoreManager.js'
 
 // Initialize
 if ( !getApps().length ) initializeApp()
 
 export const scheduledUpdateFeedDB = pubsub.schedule('every 30 minutes').onRun(async (context) => {
 	updateFeedDB()
-});
+})
+
+export const scheduledUpdateRecommendFeedDB = pubsub.schedule('every monday 00:00').onRun(async (context) => {
+	updateRecommendFeedDB()
+})
 
 export const fetchBlogTitle = https.onCall(async (data, context) => {
 	const blogTitle = await getBlogTitle(data.blogURL)
 	return {
 		blogTitle: blogTitle
 	}
-});
+})
