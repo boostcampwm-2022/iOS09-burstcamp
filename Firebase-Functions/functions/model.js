51826f7107d4e5a3cc260d96964ed7f45c77f943
miniOS <seungmin.oh.dev@gmail.com>
2022-12-02 20:45:07 +0900

    feat: rss feed데이터를 만들 때, writerUUID를 파싱해서 삽입

---
+++ Firebase-Functions/functions/model.js
@@ -0,0 +1,21 @@
+/*
+{
+  status: String
+  feed: {
+    url: 블로그 rss 주소
+    title: 블로그 이름
+    link: 블로그 주소
+    author:
+    description: 블로그 소개
+    image:
+  }
+  items: [{
+    title: 글 제목
+    pubDate: 글 작성 시간
+    link: 글 주소
+    author: 작성자 이름
+    thumbnail: 썸네일 주소
+    description: 글
+  }]
+}
+*/
--- Firebase-Functions/functions/util.js
+++ Firebase-Functions/functions/util.js
@@ -1,19 +1,19 @@
-import { MD5 } from 'crypto-js'
+import MD5 from 'crypto-js/md5.js'
 
 /// https://stackoverflow.com/a/52171480/19782341
 String.prototype.hashCode = function() {
-	const a = MD5(this).toString()
+	return MD5(this).toString()
 }
 
 /**
- * @param {string} blogLink 블로그 주소 
+ * @param {string} blogURL 블로그 주소 
  * @returns {string} 블로그 RSS주소 
  */
-export async function convertURL(blogLink) {
-	if (blogLink.includes('tistory')) {
-		return `${blogLink}/rss`
-	} else if (blogLink.includes('velog')) {
-		const nicknameCandidate = blogLink.match(/@[\w-]+/g)
+export function convertURL(blogURL) {
+	if (blogURL.includes('tistory')) {
+		return `${blogURL}/rss`
+	} else if (blogURL.includes('velog')) {
+		const nicknameCandidate = blogURL.match(/@[\w-]+/g)
 		const nickname = nicknameCandidate[nicknameCandidate.length - 1]
 		return `v2.velog.io/rss/${nickname}`
 	}
