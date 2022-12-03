b0c3aa7b9d399bc439ec7b15b881bce7e30d28d2
neuli <asdfz888@naver.com>
2022-12-03 23:08:30 +0900

    fix: 삭제

--- burstcamp/burstcamp/App/AppDelegate.swift
+++ burstcamp/burstcamp/App/AppDelegate.swift
@@ -111,8 +111,6 @@ extension AppDelegate: MessagingDelegate {
 // MARK: - save user in keychain
 
 extension AppDelegate {
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
     func applicationWillTerminate(_ application: UIApplication) {
         KeyChainManager.deleteUser()
         KeyChainManager.save(user: UserManager.shared.user)
