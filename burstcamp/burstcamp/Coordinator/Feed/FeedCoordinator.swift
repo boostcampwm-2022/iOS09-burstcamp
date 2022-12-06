1abb1686f12a448aa4e8421a04bdf7af6ae2bbb4
never-better <youtaktak@gmail.com>
2022-12-06 20:20:46 +0900

    fix: 파일 위치 이동 & 파일 생성

---
+++ burstcamp/burstcamp/Coordinator/Feed/FeedCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCoordinator.swift
+//  burstcamp
+//
+//  Created by youtak on 2022/12/06.
+//
+
+import Foundation
---
+++ burstcamp/burstcamp/Coordinator/MyPage/MyPageCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageCoordinator.swift
+//  burstcamp
+//
+//  Created by youtak on 2022/12/06.
+//
+
+import Foundation
