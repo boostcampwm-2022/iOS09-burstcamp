867b8426e3af89c2db2813b6b845bc400079ea3f
never-better <youtaktak@gmail.com>
2022-12-07 20:58:35 +0900

    feat: DefaultFirestoreFeedService 주석 추가

--- burstcamp/burstcamp/Service/Firebase/FirestoreFeedService.swift
+++ burstcamp/burstcamp/Service/Firebase/FirestoreFeedService.swift
@@ -32,12 +32,21 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
     var lastSnapShot: QueryDocumentSnapshot?
     private let paginateCount = 7
 
+    /// 최신 피드를 불러올 때 필요한 Query 생성
+    /// - Returns: pubDate 기준으로 최신 Query
+    /// @discussion
+    ///
     func createLatestQuery() -> Query {
         return FirestoreCollection.feed.reference
             .order(by: "pubDate", descending: true)
             .limit(to: paginateCount)
     }
 
+    /// 추가로 피드를 호출할 때 때 필요한 Query 생성
+    /// - Returns: lastSnapShot 다음 Document부터의 Query
+    /// @discussion
+    /// 무한 스크롤을 위한 쿼리를 생성함.
+    /// lastSnapShot이 nil 이면 마지막 피드까지 탐색한 것으로 더 이상 Query가 없기 때문에 nil을 리턴함
     func createPaginateQuery() -> Query? {
         guard let lastSnapShot = lastSnapShot
         else { return nil }
@@ -48,6 +57,10 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
             .start(afterDocument: lastSnapShot)
     }
 
+    /// 추천 피드를 호출해서 2차원 딕셔너리로 리턴함
+    /// - Returns: [[String: Any]] 타입 [Feed] 데이터
+    /// @discussion
+    /// Firestore - recommendFeed 컬렉션을 호출함
     func fetchRecommendFeedDTOs() async throws -> [[String: Any]] {
         try await withCheckedThrowingContinuation({ continuation in
             FirestoreCollection.recommendFeed.reference
@@ -57,7 +70,7 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
                         return
                     }
                     guard let querySnapShot = querySnapShot else {
-                        continuation.resume(throwing: FirebaseError.fetchFeedError)
+                        continuation.resume(throwing: FirebaseError.fetchRecommendFeedError)
                         return
                     }
                     let recommendFeeds = querySnapShot.documents.map { queryDocumentSnapshot in
@@ -69,6 +82,10 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         })
     }
 
+    /// 최신 피드를 호출해서 2차원 딕셔너리로 리턴함
+    /// - Returns: [[String: Any]] 타입 [Feed] 데이터
+    /// @discussion
+    /// Firestore - feed 컬렉션에서 pubDate 기준으로 paginateCount만큼 호출합니다.
     func fetchLatestFeedDTOs() async throws -> [[String: Any]] {
         try await withCheckedThrowingContinuation({ continuation in
             let feedQuery = createLatestQuery()
@@ -98,6 +115,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         })
     }
 
+    /// 최신 피드 이후에 추가적인 피드를 호출해서 2차원 딕셔너리로 리턴함
+    /// - Returns: [[String: Any]] 타입 [Feed] 데이터
+    /// @discussion
+    /// fetchLatestFeedDTOs() 이후에 호출합니다.
+    /// Firestore - feed 컬렉션에서  pubDate 기준으로 paginateCount만큼 추가 호출합니다.
     func fetchMoreFeeds() async throws -> [[String: Any]] {
         try await withCheckedThrowingContinuation({ continuation in
             guard let feedQuery = createPaginateQuery() else {
@@ -131,6 +153,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         })
     }
 
+    /// firestore - feed 컬렉션에서 feed 1개를 호출함
+    /// - Parameters:
+    ///   - feedUUID: firestore에서 사용하는 feedUUID
+    /// - Returns: [String: Any] 타입 Feed 데이터
+    /// @discussion
     func fetchFeedDTO(feedUUID: String) async throws -> [String: Any] {
         try await withCheckedThrowingContinuation({ continuation in
             FirestoreCollection.feed.reference
@@ -151,6 +178,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         })
     }
 
+    /// firestore - user 컬렉션에서 user 1명을 호출함
+    /// - Parameters:
+    ///   - userUUID: firestore에서 사용하는 userUUID
+    /// - Returns: [String: Any] 타입 User 데이터
+    /// @discussion
     func fetchUser(userUUID: String) async throws -> [String: Any] {
         try await withCheckedThrowingContinuation({ continuation in
             FirestoreCollection.user.reference
@@ -171,6 +203,12 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         })
     }
 
+    /// Firestore 스크랩 카운트를 호출해서 리턴함
+    /// - Parameters:
+    ///   - feedUUID: firestore에서 사용하는 feedUUID
+    /// - Returns: 스크랩 카운트
+    /// @discussion
+    /// firestore - feed - `feedUUID` - scrapUser 의 카운트를 세서 리턴해준다.
     func countFeedScarp(feedUUID: String) async throws -> Int {
         let path = FirestoreCollection.scrapUser(feedUUID: feedUUID).path
         let countQuery = Firestore.firestore().collection(path).count
@@ -178,6 +216,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         return Int(truncating: collectionCount.count)
     }
 
+    /// 스크랩시 호출해서 Feed의 scrapUser에 User 데이터를 추가함
+    /// - Parameters:
+    ///   - userUUID: Feed - scrapUser에 추가할 userUUID
+    ///   - feedUUID: user를 추가할 Feed의 UUID
+    /// @discussion
     func appendUserToFeed(userUUID: String, at feedUUID: String) async throws {
         try await withCheckedThrowingContinuation { continuation in
             FirestoreCollection.scrapUser(feedUUID: feedUUID).reference
@@ -195,6 +238,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         } as Void
     }
 
+    /// 스크랩 해제시 호출해서 Feed의 scrapUser에 User 데이터를 삭제함
+    /// - Parameters:
+    ///   - userUUID: Feed - scrapUser에서 삭제할 userUUID
+    ///   - feedUUID: user를 삭제할 Feed의 UUID
+    /// @discussion
     func deleteUserFromFeed(userUUID: String, at feedUUID: String) async throws {
         try await withCheckedThrowingContinuation { continuation in
             FirestoreCollection.scrapUser(feedUUID: feedUUID).reference
@@ -209,6 +257,11 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         } as Void
     }
 
+    /// 스크랩시 호출해서 user의 scrapFeedUUIDs에 FeedUUID를 추가함
+    /// - Parameters:
+    ///   - feedUUID: user의  scrapFeedUUIDs에 추가할 Feed의 UUID
+    ///   - userUUID: Feed를 추가할 user의 userUUID
+    /// @discussion
     func appendFeedUUIDToUser(feedUUID: String, at userUUID: String) async throws {
         try await withCheckedThrowingContinuation { continuation in
             FirestoreCollection.user.reference
@@ -225,6 +278,10 @@ final class DefaultFirestoreFeedService: FirestoreFeedService {
         } as Void
     }
 
+    /// 스크랩 해제시 호출해서 user의 scrapFeedUUIDs에 FeedUUID를 삭제함
+    /// - Parameters:
+    ///   - feedUUID: user의  scrapFeedUUIDs에 삭제할 Feed의 UUID
+    ///   - userUUID: Feed를 삭제할 user의 userUUID
     func deleteFeedUUIDFromUser(feedUUID: String, at userUUID: String) async throws {
         try await withCheckedThrowingContinuation { continuation in
             FirestoreCollection.user.reference
--- burstcamp/burstcamp/Util/Error/FirebaseError.swift
+++ burstcamp/burstcamp/Util/Error/FirebaseError.swift
@@ -8,23 +8,32 @@
 import Foundation
 
 enum FirebaseError: LocalizedError {
+
+    /// 유저
     case fetchUserError
-    case fetchFeedError
-    case fetchScrapCountError
-    case lastFetchError
-    case paginateQueryError
     case userDeleteError
     case userSignOutError
 
+    /// 피드
+    case fetchFeedError
+    case fetchRecommendFeedError
+    case fetchScrapCountError
+    case lastFetchError
+    case paginateQueryError
+
     var errorDescription: String? {
         switch self {
+        /// 유저
         case .fetchUserError: return "유저를 불러오던 중 에러가 발생했습니다."
+        case .userDeleteError: return "유저를 삭제하던 중 에러가 발생했습니다."
+        case .userSignOutError: return "로그아웃 하던 중 에러가 발생했습니다."
+
+        /// 피드
         case .fetchFeedError: return "피드를 불러오던 중 에러가 발생했습니다."
+        case .fetchRecommendFeedError: return "추천 피드를 불러오던 중 에러가 발생했습니다."
         case .fetchScrapCountError: return "스크랩 수를 불러오던 중 에러가 발생했습니다."
         case .lastFetchError: return "마지막 스크랩입니다."
         case .paginateQueryError: return "피드를 추가로 불러오던 중 에러가 발생했습니다."
-        case .userDeleteError: return "유저를 삭제하던 중 에러가 발생했습니다."
-        case .userSignOutError: return "로그아웃 하던 중 에러가 발생했습니다."
         }
     }
 }
