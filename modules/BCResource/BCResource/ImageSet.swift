ffc4fb1c407a4f415d8d21f215575695b7b3cbdf
miniOS <seungmin.oh.dev@gmail.com>
2022-12-07 17:27:24 +0900

    feat: Resource 모듈 생성

---
+++ modules/BCResource/BCResource/ImageSet.swift
@@ -0,0 +1,12 @@
+//
+//  ImageSet.swift
+//  BCResource
+//
+//  Created by SEUNGMIN OH on 2022/12/07.
+//
+
+import UIKit
+
+public extension UIImage {
+    static let burstcamper = Assets.Image.burstcamper100.image
+}
