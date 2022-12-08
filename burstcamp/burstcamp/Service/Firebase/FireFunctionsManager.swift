303fe133de5ab171e7a6e7e4ce07182b5cc40ed6
neuli <asdfz888@naver.com>
2022-12-08 21:19:42 +0900

    feat: firefunctions 유저 정보 삭제 함수 생성 #207

--- burstcamp/burstcamp/Service/Firebase/FireFunctionsManager.swift
+++ burstcamp/burstcamp/Service/Firebase/FireFunctionsManager.swift
@@ -14,17 +14,39 @@ struct FireFunctionsManager {
 
     static var functions = Functions.functions()
 
+    static let fetchBlogTitle = "fetchBlogTitle"
+    static let deleteUser = "deleteUser"
+    static let blogTitleField = "blogTitle"
+    static let userUUIDFeild = "userUUID"
+
     static func blogTitle(link: String) -> Future<String, NetworkError> {
         return Future<String, NetworkError> { promise in
             functions
-                .httpsCallable("fetchBlogTitle")
-                .call(["blogURL": link]) { result, error in
+                .httpsCallable(fetchBlogTitle)
+                .call([blogTitleField: link]) { result, error in
                     if error != nil {
                         promise(.failure(NetworkError.unknownError))
                     }
                     if let data = result?.data as? [String: Any],
-                       let blogTitle = data["blogTitle"] as? String {
+                       let blogTitle = data[blogTitleField] as? String {
                         promise(.success(blogTitle))
+                    }
+                    promise(.failure(NetworkError.unknownError))
+                }
+        }
+    }
+
+    static func deleteUser(userUUID: String) -> Future<Bool, NetworkError> {
+        return Future<Bool, NetworkError> { promise in
+            functions
+                .httpsCallable(deleteUser)
+                .call([userUUIDFeild: userUUID]) { result, error in
+                    if error != nil {
+                        promise(.failure(NetworkError.unknownError))
+                    }
+                    if let data = result?.data as? [String: Any],
+                       let isFinish = data[userUUIDFeild] as? Bool {
+                        promise(.success(isFinish))
                     }
                     promise(.failure(NetworkError.unknownError))
                 }
