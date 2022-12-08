ada6ec5aa030d48c42b737caca7cea4247001317
never-better <youtaktak@gmail.com>
2022-12-08 16:11:10 +0900

    feat: Error Extension 에 resolveError 추가
    ErrorCategory를 리턴함

---
+++ burstcamp/burstcamp/Util/Error/Error+resolveCategory.swift
@@ -0,0 +1,17 @@
+//
+//  Error+resolveCategory.swift
+//  burstcamp
+//
+//  Created by youtak on 2022/12/08.
+//
+
+import Foundation
+
+extension Error {
+    func resolveError() -> ErrorCategory {
+        guard let category = self as? CategorizedError else {
+            return .nonRetryable
+        }
+        return category.category
+    }
+}
