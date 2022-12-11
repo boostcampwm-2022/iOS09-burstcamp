90a451ea2d70659e1d834640fa8b17882c5ce56b
miniOS <seungmin.oh.dev@gmail.com>
2022-12-11 16:16:24 +0900

    feat: Fetcher 모듈 구현

---
+++ modules/BCFetcher/BCFetcher/BCFetcher.h
@@ -0,0 +1,18 @@
+//
+//  BCFetcher.h
+//  BCFetcher
+//
+//  Created by SEUNGMIN OH on 2022/12/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BCFetcher.
+FOUNDATION_EXPORT double BCFetcherVersionNumber;
+
+//! Project version string for BCFetcher.
+FOUNDATION_EXPORT const unsigned char BCFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BCFetcher/PublicHeader.h>
+
+
---
+++ modules/BCFetcher/BCFetcher/FetchingState.swift
@@ -0,0 +1,14 @@
+//
+//  FetchingState.swift
+//  BCFetcher
+//
+//  Created by SEUNGMIN OH on 2022/12/08.
+//
+
+import Foundation
+
+public enum Status<FetchingError: Swift.Error> {
+    case loading
+    case failure(_ error: FetchingError)
+    case success
+}
