fbf0af5876420b494dafda8af7400cd1d3dff376
miniOS <seungmin.oh.dev@gmail.com>
2022-12-11 18:09:24 +0900

    feat: AnyCancellage + store method

---
+++ modules/BCFetcher/Extension/AnyCancellable+store.swift
@@ -0,0 +1,16 @@
+//
+//  AnyCancellable + store.swift
+//  BCFetcher
+//
+//  Created by SEUNGMIN OH on 2022/12/11.
+//
+
+import Foundation
+
+public extension Set where Element == AnyCancellable {
+    func store(in set: inout Self) {
+        self.forEach { cancellable in
+            cancellable.store(in: &set)
+        }
+    }
+}
