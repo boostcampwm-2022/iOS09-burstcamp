79852ae7828954ddea68a172272a9df199a92013
miniOS <seungmin.oh.dev@gmail.com>
2022-12-13 17:43:53 +0900

    chore: typealias 추가

--- modules/Manager/RealmManager/RealmManager/RealmCompatible.swift
+++ modules/Manager/RealmManager/RealmManager/RealmCompatible.swift
@@ -5,11 +5,9 @@
 //  Created by SEUNGMIN OH on 2022/12/13.
 //
 
-import Foundation
-
 import RealmSwift
 
-/// 구조체를 RealmSwift에서 사용할 수 있도록 래핑해주는 protocol
+/// 구조체를 RealmSwift에서 사용할 수 있도록 래핑해주는 `protocol`
 public protocol RealmCompatible {
     associatedtype RealmModel: RealmSwift.Object
     associatedtype PropertyValue: PropertyValueType
@@ -19,16 +17,22 @@ public protocol RealmCompatible {
     func realmModel() -> RealmModel
 }
 
-/// 데이터를 type-safe 하게 사용할 수 있도록 도와주는 typealias
+/// 데이터를 type-safe 하게 사용할 수 있도록 도와주는 `typealias`
 public typealias PropertyValuePair = (name: String, value: Any)
 
-/// 데이터를 type-safe 하게 사용할 수 있도록 도와주는 protocol
+/// 데이터를 type-safe 하게 사용할 수 있도록 도와주는 `protocol`
 public protocol PropertyValueType {
     var propertyValuePair: PropertyValuePair { get }
 }
 
+/// Foundation의 SortDescriptor과 구별하기 위한 `typealias`
+///
+/// @discussion
+/// 이를 통해, 상위 모듈에서 "렐름"을 Import하지 않아도 된다.
+public typealias RealmSortDescriptor = RealmSwift.SortDescriptor
+
 /// Query를 사용할 수 있도록 도와주는 protocol
 public protocol QueryType {
     var predicate: NSPredicate? { get }
-    var sortDescriptors: [RealmSwift.SortDescriptor] { get }
+    var sortDescriptors: [RealmSortDescriptor] { get }
 }
