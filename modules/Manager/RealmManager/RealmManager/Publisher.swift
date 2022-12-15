db8bbf3ab7b30a7d337314d64842608aebc05f6d
miniOS <seungmin.oh.dev@gmail.com>
2022-12-15 12:48:11 +0900

    feat: Diff를 방출하는 Publisher 추가

--- modules/Manager/RealmManager/RealmManager/Publisher.swift
+++ modules/Manager/RealmManager/RealmManager/Publisher.swift
@@ -9,12 +9,53 @@ import struct Combine.AnyPublisher
 
 import protocol RealmSwift.RealmFetchable
 import struct RealmSwift.Results
+import protocol RealmSwift.RealmCollection
+import RealmSwift
 
 extension Container {
-    public func publisher<T: RealmFetchable>(_ type: T.Type)
+    public func collectionPublisher<T: RealmFetchable>(_ type: T.Type)
     -> AnyPublisher<Results<T>, Error> {
         return object(type)
             .collectionPublisher
+            .threadSafeReference()
+            .eraseToAnyPublisher()
+    }
+
+    /// Collection의 변경사항들을 방출하는 publisher
+    ///
+    /// 변경사항들을 insertion, deletion, modification으로 분류해 반영해야 할 때 사용.
+    ///
+    /// - SeeAlso:
+    ///
+    /// ```swift
+    ///     container.publisher(RealmModel.self)
+    ///         .sink { [weak self] (changes: RealmCollectionChange) in
+    ///             guard let tableView = self?.tableView else { return }
+    ///             
+    ///             switch changes {
+    ///             case .initial:
+    ///                 tableView.reloadData()
+    ///             case .update(_, let deletions, let insertions, let modifications):
+    ///
+    ///             tableView.performBatchUpdates({
+    ///                 tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) },
+    ///                                      with: .automatic)
+    ///                 tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) },
+    ///                                      with: .automatic)
+    ///                 tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) },
+    ///                                      with: .automatic)
+    ///             }, completion: { finished in
+    ///                 print("tableView Update 완료")
+    ///             })
+    ///         }
+    ///         .store(in: &cancelBag)
+    /// ```
+
+
+    public func publisher<T: RealmFetchable>(_ type: T.Type)
+    -> AnyPublisher<RealmCollectionChange<Results<T>>, Never> {
+        return object(type)
+            .changesetPublisher
             .subscribe(on: serialQueue)
             .threadSafeReference()
             .eraseToAnyPublisher()
