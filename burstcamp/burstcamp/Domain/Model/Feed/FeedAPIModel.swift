6acbb885bdb73b3b1f422b44393b0825958dadee
never-better <youtaktak@gmail.com>
2023-01-07 16:08:44 +0900

    feat: FeedAPI 모델 수정
    writer 관련 내용 추가

--- burstcamp/burstcamp/Domain/Model/Feed/FeedAPIModel.swift
+++ burstcamp/burstcamp/Domain/Model/Feed/FeedAPIModel.swift
@@ -11,23 +11,33 @@ import class FirebaseFirestore.Timestamp
 
 struct FeedAPIModel {
     let feedUUID: String
-    let writerUUID: String
     let title: String
     let pubDate: Date
     let url: String
     let thumbnailURL: String
     let content: String
+    let writerCamperID: String
+    let writerDomain: String
+    let writerNickname: String
+    let writerOrdinalNumber: Int
+    let writerProfileImageURL: String
+    let writerUUID: String
 }
 
 extension FeedAPIModel {
     init(data: [String: Any]) {
         self.feedUUID = data["feedUUID"] as? String ?? ""
-        self.writerUUID = data["writerUUID"] as? String ?? ""
         self.title = data["title"] as? String ?? ""
         let timeStampDate = data["pubDate"] as? Timestamp ?? Timestamp()
         self.pubDate = timeStampDate.dateValue()
         self.url = data["url"] as? String ?? ""
-        self.thumbnailURL = data["thumbnail"] as? String ?? ""
+        self.thumbnailURL = data["thumbnailURL"] as? String ?? ""
         self.content = data["content"] as? String ?? ""
+        self.writerCamperID = data["writerCamperID"] as? String ?? ""
+        self.writerDomain = data["writerDomain"] as? String ?? ""
+        self.writerNickname = data["writerNickname"] as? String ?? ""
+        self.writerOrdinalNumber = data["writerOrdinalNumber"] as? Int ?? 7
+        self.writerProfileImageURL = data["writerProfileImageURL"] as? String ?? ""
+        self.writerUUID = data["writerUUID"] as? String ?? ""
     }
 }
