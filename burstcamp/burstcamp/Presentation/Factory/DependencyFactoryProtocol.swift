ba34ba2d20c677957f4d78c3855794e301e01e1b
never-better <youtaktak@gmail.com>
2023-01-14 23:34:57 +0900

    feat: DependencyFactoryProtocol

---
+++ burstcamp/burstcamp/Presentation/Factory/DependencyFactoryProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  DependencyFactoryProtocol.swift
+//  burstcamp
+//
+//  Created by youtak on 2023/01/14.
+//
+
+import Foundation
+
+protocol DependencyFactoryProtocol {
+    func createLoginViewModel() -> LogInViewModel
+    func createSignUpDomainViewModel() -> SignUpDomainViewModel
+    func createSignUpCamperIDViewModel() -> SignUpCamperIDViewModel
+    func createSignUpBlogViewModel() -> SignUpBlogViewModel
+    func createHomeViewModel() -> HomeViewModel
+    func createScrapPageViewModel() -> ScrapPageViewModel
+    func createFeedDetailViewModel() -> FeedDetailViewModel
+    func createMyPageViewModel() -> MyPageViewModel
+    func createMyPageEditViewModel() -> MyPageEditViewModel
+}
