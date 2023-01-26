b92046ed9de953d566ca351e882532e2d804f22a
never-better <youtaktak@gmail.com>
2023-01-26 15:04:37 +0900

    feat: UILabel에 baseLineOffset 적용

--- burstcamp/burstcamp/Util/Extension/UI/View/UILabel+LineHeight.swift
+++ burstcamp/burstcamp/Util/Extension/UI/View/UILabel+LineHeight.swift
@@ -17,10 +17,17 @@ extension UILabel {
         style.maximumLineHeight = lineHeight
         style.minimumLineHeight = lineHeight
 
+        let baseLineOffset = (lineHeight - font.lineHeight) / 2.0 / 2.0
+
         let attributes: [NSAttributedString.Key: Any] = [
-            .paragraphStyle: style
+            .paragraphStyle: style,
+            .baselineOffset: baseLineOffset
         ]
 
         self.attributedText = NSAttributedString(string: labelText, attributes: attributes)
     }
 }
+
+// baslineOffset 참고
+//https://sujinnaljin.medium.com/swift-label%EC%9D%98-line-height-%EC%84%A4%EC%A0%95-%EB%B0%8F-%EA%B0%80%EC%9A%B4%EB%8D%B0-%EC%A0%95%EB%A0%AC-962f7c6e7512
+//http://blog.eppz.eu/uilabel-line-height-letter-spacing-and-more-uilabel-typography-extensions/
