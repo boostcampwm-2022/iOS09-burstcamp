bcde7b6e4f4920d0b0018601d15fe60b37934a1b
never-better <youtaktak@gmail.com>
2023-02-03 12:43:51 +0900

    feat: BCFirebaseMessaging 구현

--- burstcamp/burstcamp/Service/Firebase/Messaging/BCFirebaseMessaging.swift
+++ burstcamp/burstcamp/Service/Firebase/Messaging/BCFirebaseMessaging.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+import Firebase
+
+protocol BCFirebaseMessagingDelegate: AnyObject {
+    func configureMessaging(token: String?)
+    func refreshToken(token: String?)
+}
+
+final class BCFirebaseMessaging: NSObject, MessagingDelegate {
+
+    private let messaging: Messaging
+    weak var delegate: BCFirebaseMessagingDelegate?
+
+    init(messaging: Messaging = Messaging.messaging()) {
+        self.messaging = messaging
+    }
+
+    func saveApnsToken(apnsToken: Data) {
+        messaging.apnsToken = apnsToken
+    }
+
+    func configureMessaging() throws {
+        messaging.delegate = self
+        Task { [weak self] in
+            do {
+                let token = try await self?.messaging.token()
+                self?.delegate?.configureMessaging(token: token)
+            } catch {
+                debugPrint("BCFirebaseMessaging - configureMessaging 에러")
+                throw BCFirebaseMessagingError.configureMessaging
+            }
+        }
+    }
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcmToken = fcmToken {
+            delegate?.refreshToken(token: fcmToken)
+        }
+    }
+}
+
+enum BCFirebaseMessagingError: Error {
+    case configureMessaging
+    case refreshToken
+}
